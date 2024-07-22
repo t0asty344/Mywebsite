72eba43b6dbb92e58e681e6504b81ba333a3ee5b
toasty <okiuhm77@gmail.com>
2024-07-22 08:12:23 +0200

    prob will cause issues commit

--- public/threejs/main.js
+++ public/threejs/main.js
@@ -1,68 +1,140 @@
-
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import './threestyle.css'
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+const aspect = window.innerWidth / window.innerHeight;
+const d = 10;
+const camera = new THREE.OrthographicCamera(-d * aspect, d * aspect, d, -d, 1, 1000);
+camera.position.set(20, 20, 20); // Position the camera
+
+const loader = new GLTFLoader();
+const renderer = new THREE.WebGLRenderer( {canvas:document.querySelector('#bg')});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
+camera.position.set(9,9,80)
 
-let cor_array = [[3,-3],[-3,3],[-3,-3],[3,3]]
+let importobs;
 
+function importss(x=0,y=0,z=-10,path='3d/firstweb1.glb')
+{
+    let importobj;
+    let importobjarray = [];
+    loader.load( path, function ( gltf ) {
+        importobj = gltf.scene
+        scene.add( gltf.scene );
+        
+        importobj.position.set(x,y,z);
+        importobj.rotation.set(1,1.6,6.9)
+        importobjarray.push(importobj);
+
+    }, undefined, function ( error ) {
+    
+        console.error( error );
+    
+    } );
+}
+
+var counter =0;
+ function  creategrid(){
+    let int1 = THREE.MathUtils.randInt(1,10)
+    let int2 = THREE.MathUtils.randInt(1,10)
+    for(var i=0; i<int1;i++){
+        for(var e=0; e<int2;e++)
+        {
+            importss(e+e,i+i)
+        }
+        }
+    }
+    
+
+creategrid()
+let cor_array = [[7,3],[-3,7],[3,-7],[-7,-3]]
+
+//first sphere
 let cubex = 0
 let cubey= 0
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const texture = new THREE.TextureLoader().load('/images/nebula.jpg')
+const geometry = new THREE.SphereGeometry( 6, 32, 32 );
+const material = new THREE.MeshBasicMaterial( { map:texture } );
 const cube = new THREE.Mesh( geometry, material );
 cube.position.set(cubex,cubey,0)
 scene.add( cube );
 
-let cube2x = 2.5
-let cube2y = 1.25
-var pivotc2 = new THREE.Object3D();
-const geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
-const material2 = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-const cube2 = new THREE.Mesh( geometry2, material2 );
 
+//second cube
+let cube2x = 6.5
+let cube2y = 3.25
+
+const geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
+const material2 = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+const cube2 = new THREE.Mesh( geometry2, material2 );
+//pivot
+var pivotc2 = new THREE.Object3D();
 cube.add(pivotc2)
 pivotc2.position.set(0,0,0)
+//adding cube to pivot
 pivotc2.add(cube2);
 cube2.position.set(cube2x,cube2y,0);
+
+//light
+const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+scene.add( light );
+
+//Helpers
+const lightHelp = new THREE.HemisphereLightHelper(light)
+const gridHelper = new THREE.GridHelper(200,50);
+scene.add(gridHelper)
+scene.add(lightHelp)
+
+//OrbitalControls
+const control = new OrbitControls(camera,renderer.domElement);
 
 for(var i=0;i<4;i++)
 {
     
-    dynamic_shape(cor_array[i][0],cor_array[i][1])
+dynamic_shape(cor_array[i][0],cor_array[i][1])
+
 }
 
+const background = new THREE.TextureLoader().load('/images/space.jpg') 
+scene.background = background
 
-
-camera.position.z = 5;
 document.addEventListener("keydown",(event)=>{
    var key = event.key
     switch(event.key) 
     {
         case "a":
-            camera.position.x -=0.1;
+            camera.position.x -=2;
             console.log("hello")
             break;
         case "d":
-            camera.position.x +=0.1;
+            camera.position.x +=2;
             break;
         case "w":
-            camera.position.y +=0.1;
+            camera.position.y +=2;
             break;
         case "s":
-            camera.position.y -=0.1;
+            camera.position.y -=2;
             break;
     }
 })
+
 function animate() {
 	requestAnimationFrame( animate );
+    cube.rotation.y +=0.001
+    cube2.scale.x += 0.1
+    cube2.scale.x -=0.1 
+    pivotc2.rotation.z +=0.0001;
+    cube2.rotation.x +=0.05;
+    cube2.rotation.y +=0.05;
+    control.update();
+    renderer.render(scene, camera );
     
-    pivotc2.rotation.z +=0.01;
-    console.log(pivotc2.rotation)
-	renderer.render(scene, camera );
+	
 }
 
 function object_animation(object_name,object_movementx=0,object_movementy=0,object_rotationx=0,object_rotationy=0)
@@ -71,17 +143,16 @@ function object_animation(object_name,object_movementx=0,object_movementy=0,obje
     object_name.rotation.y += object_rotationy
     object_name.position.x += new THREE.Vector3(object_movementx,object_movementy,0)
 }
+
 function dynamic_shape(postionx=0,positiony=0){
     var pivotd = new THREE.Object3D();
-    const geometryd = new THREE.BoxGeometry( 1, 1, 1 );
-    const materiald= new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    const cubed = new THREE.Mesh( geometry2, material2 );
+    const geometryd = new THREE.BoxGeometry( 0.5, 2, 1 );
+    const materiald= new THREE.MeshBasicMaterial( { color: 0x000000 } );
+    const cubed = new THREE.Mesh( geometryd, materiald );
+    
+    pivotc2.add(cubed)
+    cubed.rotation.set(1,90,1);
     cubed.position.set(postionx,positiony,0)
-    scene.add(cubed)
 }
 
-document.addEventListener("mousedown",(event)=>{
-    
-    mouse.x
-})
 animate();
