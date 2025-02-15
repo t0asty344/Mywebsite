9a15e52d40d1c0c5a63631870c1ab1f59011b88b
toasty <okiuhm77@gmail.com>
2025-02-15 10:27:02 +0100

    back at it

--- src/Documentation.js
+++ src/Documentation.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 import "../public/styles.css";
-import { append } from "three/examples/jsm/nodes/Nodes.js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_APIKEY;
--- src/todo.js
+++ src/todo.js
@@ -14,10 +14,13 @@ function add(div,input)
     let sect = document.createElement("select")
     let option1 = document.createElement("option")
     let newbutton = document.createElement("button") 
+    let editbutton = document.createElement("button")
+    editbutton.innerHTML="edit"
     para.innerHTML = input
     option1.innerHTML= "finished"
     newbutton.innerHTML = "X"
     //classes
+    para.classList.add("textleft")
     newdiv.classList.add("todolistitem")
     sect.classList.add("selectstatus")
 
@@ -25,6 +28,7 @@ function add(div,input)
     div.append(newlistelement)
     newlistelement.append(newdiv)
     newdiv.append(para)
+    newdiv.append(editbutton)
     newdiv.append(sect)
     newdiv.append(newbutton)
     sect.append(option1)
@@ -32,32 +36,109 @@ function add(div,input)
     {
         newlistelement.remove()
     })
+    editbutton.addEventListener("click",()=>{
+        edit(editbutton,para)
+    })
 }
-function addlist()
+function addlist(titleinput,onload)
 {
     let tododiv = document.getElementById("todolistsdiv")
+    let titleh1 = document.createElement("h1"); 
     let newtodolist = document.createElement("div")
     let newtodobutton = document.createElement("button")
     let input = document.createElement("input")
     let newlist = document.createElement("ul")
+    titleh1.innerHTML = titleinput
     newtodobutton.innerHTML = "add todo"
     newtodolist.classList.add("tododiv")
+    newtodolist.classList.add("todolistdiv")
+    titleh1.classList.add("marginleft")
+    tododiv.append(titleh1)
     tododiv.append(newtodolist)
     newtodolist.append(newtodobutton)
     newtodolist.append(input)
     newtodolist.append(newlist)
     newtodobutton.addEventListener("click",()=>{
-        add(newlist,input.vlaue);
+        if(input.value=="")
+        {
+            window.alert("please dont leave it empty")
+        }
+        else
+        {
+        inputlistdb(input.value,"inprogress",titleinput);
+        add(newlist,input.value);
+        input.value="";
+        }
+    if(onload==true)
+    {
+        loadlist(newlist,titleinput)
+    }
     })
     
     
 }
 function edit(editbutton,edittext)
 {
-    document.getElementById("editbutton")
-    document.getElementById()
+    editbutton
+    edittext
 }
+//Database shit
+//inserts
+async function inputsubjectdb(title)
+{
+    const{ error } = await supabase
+    .from("todolistsubject")
+    .insert({Subject:title})
+    if (error) {
+        console.log(error);
+      }
+}
+async function inputlistdb(todotext,statustext,subject)
+{
+    const{ error } = await supabase
+    .from("todotable")
+    .insert({todo:todotext,status:statustext,Subject:subject})
+    if (error) {
+        console.log(error);
+      }
+}
+//Selects
+async function loadlist(div,Subject){
+    const{data,error} = await supabase
+    .from("todotable")
+    .select("*")
+    .eq("Subject",Subject)
+    if (error) {
+        console.log(error);
+      }
+      data.forEach((dat)=>{add(div,dat.Subject)})
+}
+
+async function loadSubjects()
+{
+const{data,error} = await supabase
+    .from("todolistsubject")
+    .select("*")
+    if (error) {
+        console.log(error);
+      }
+      data.forEach((dat)=>{isload(dat.Subject)})
+}
+function isload(info)
+{
+    addlist(info,true);
+}
+loadSubjects()
 document.getElementById("addelementbutton").addEventListener("click",add)
 document.getElementById("addlistelementbutton").addEventListener("click",()=>{
-    addlist();
+    let inputvalue =document.getElementById("addlistelementtext").value
+    if(inputvalue=="")
+    {
+        window.alert("please dont leave it empty")
+    } 
+    else
+    {
+    inputsubjectdb(inputvalue)
+    addlist(inputvalue);
+    }
 })
